ba5227f299004da38230f567d43783dfde2b38a3
Victor Manuel <153263915+Mercafed@users.noreply.github.com>
2026-01-11 12:36:02 -0500

    Refactor build.gradle.kts for versioning and properties
    Updated version number and modified cloudstream properties.
    error al no seleccionar bien un archivo corregido

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,13 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+plugins {
+    kotlin("jvm")
 }
 
-// Use an integer for version numbers
-version = 1
+version = "1.0.0"
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
-    }
+    language = "es"
+    authors = listOf("Mercafed")
+    status = 1
+    tvTypes = listOf("Anime")
+    iconUrl = "https://seriesdonghua.com/favicon.ico"
 }
